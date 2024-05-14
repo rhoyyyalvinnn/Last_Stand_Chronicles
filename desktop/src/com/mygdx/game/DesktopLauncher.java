a2cd085bd6cbd13886095d2d86ebf37b3e036761
Pielago Family <emmanroy.pielago25@gmail.com>
2024-05-14 20:02:53 +0800

    logo

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Last Stand Chronicles");
+		config.setWindowIcon("resources/logo_2.png");
 		new Lwjgl3Application((ApplicationListener) new MyGdxGame(), config);
 	}
 }

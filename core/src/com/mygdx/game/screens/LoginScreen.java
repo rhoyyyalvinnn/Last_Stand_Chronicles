8062fdf003b613ceb4996f877d078064a23c53d4
manroy <emmanroy.pielago03@gmail.com>
2024-05-22 02:50:57 +0800

    nice ui

--- core/src/com/mygdx/game/screens/LoginScreen.java
+++ core/src/com/mygdx/game/screens/LoginScreen.java
@@ -8,11 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -31,18 +27,23 @@ public class LoginScreen implements Screen {
     private Stage stage;
     private Skin skin;
     private MyGdxGame context;
-    private SpriteBatch batch;
+    private SpriteBatch batch,imagebatch;
     private Texture background;
     private ShapeRenderer shapeRenderer;
     private Label actionTarget;
+    private Texture bgTexture;
+    private Image imageBg;
 
     public LoginScreen(MyGdxGame context) {
         this.context = context;
         stage = new Stage(new ScreenViewport());
-        skin = new Skin(Gdx.files.internal("sample.json"));
+        skin = new Skin(Gdx.files.internal("skinfiles/last_stand2.json"));
         batch = new SpriteBatch();
+        imagebatch=new SpriteBatch();
         background = new Texture(Gdx.files.internal("loading/balo.png"));
         shapeRenderer = new ShapeRenderer();
+        bgTexture = new Texture(Gdx.files.internal("skinfiles/bg_login.png"));
+        imageBg=new Image(bgTexture);
     }
 
     @Override
@@ -63,25 +64,33 @@ public class LoginScreen implements Screen {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
-
+        float rectangleWidth = 1650;
+        float rectangleHeight = 900;
+        imageBg.setPosition(((Gdx.graphics.getWidth() - rectangleWidth) / 2),(Gdx.graphics.getHeight() - rectangleHeight) / 2);
+        imageBg.setSize(rectangleWidth, rectangleHeight);
+        stage.addActor(imageBg);
 
         Gdx.input.setInputProcessor(stage);
 
         Table table = new Table();
         table.setFillParent(true);
+
         stage.addActor(table);
 
-        Label usernameLabel = new Label("Username:", skin);
+
+        Label usernameLabel = new Label("  Username:  ", skin);
         final TextField usernameField = new TextField("", skin);
 
-        Label passwordLabel = new Label("Password:", skin);
+        Label passwordLabel = new Label("  Password:  ", skin);
         final TextField passwordField = new TextField("", skin);
+
         passwordField.setPasswordCharacter('*');
         passwordField.setPasswordMode(true);
-        actionTarget  = new Label("", skin,"font", Color.RED);
-        TextButton loginButton = new TextButton("Login", skin);
+       // passwordField.setAlignment(3);
+       // passwordField.
+        actionTarget  = new Label("", skin,"kapoya", Color.RED);
+
+        TextButton loginButton = new TextButton(" Login ", skin);
         loginButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -92,13 +101,13 @@ public class LoginScreen implements Screen {
                 if (authenticate(username, password)) {
                     context.setScreen(new MenuScreen(context)); // Switch to the menu screen on successful login
                 } else {
-                    actionTarget.setText("Invalid Username or password");
+                    actionTarget.setText(" Invalid Username or password ");
                 }
             }
         });
 
         // Add clickable label for registration
-        Label registerLabel = new Label("No Account? Register here", skin, "font", Color.BLUE);
+        Label registerLabel = new Label("  No Account? Register here  ", skin, "kapoya", Color.BLUE);
         registerLabel.setAlignment(Align.center);
         registerLabel.addListener(new ClickListener() {
             @Override
@@ -111,13 +120,13 @@ public class LoginScreen implements Screen {
 
         // Position the form elements within the table
         table.center();
-        table.add(usernameLabel).pad(10);
-        table.add(usernameField).width(200).pad(10);
+        table.add(usernameLabel).height(100).pad(10);
+        table.add(usernameField).width(600).height(100).pad(10);
         table.row();
-        table.add(passwordLabel).pad(10);
-        table.add(passwordField).width(200).pad(10);
+        table.add(passwordLabel).height(100).pad(10);
+        table.add(passwordField).width(600).height(100).pad(10);
         table.row();
-        table.add(loginButton).colspan(2).pad(10).row();
+        table.add(loginButton).height(120).colspan(2).pad(10).row();
         table.add(registerLabel).colspan(2).pad(10).center().bottom();
         table.row();
         table.add(actionTarget).colspan(2).pad(10).center().bottom();
@@ -156,18 +165,19 @@ public class LoginScreen implements Screen {
 
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
         batch.end();
 
         // Calculate position and draw the rectangle behind the form
-        float rectangleWidth = 500;
-        float rectangleHeight = 350;
+      /*  float rectangleWidth = 1200;
+        float rectangleHeight = 600;
         float rectangleX = (Gdx.graphics.getWidth() - rectangleWidth) / 2;
         float rectangleY = (Gdx.graphics.getHeight() - rectangleHeight) / 2;
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(new Color(1, 1, 1, 0.7f)); // Semi-transparent white
         shapeRenderer.rect(rectangleX, rectangleY, rectangleWidth, rectangleHeight);
-        shapeRenderer.end();
+        shapeRenderer.end();*/
 
         stage.act(delta);
         stage.draw();
